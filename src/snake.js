0e84f11c9a67092c09a48755ec2554494e294bf0
Mohsen <moh.1380.1393@gmail.com>
2022-01-18 18:28:21 +0330

    add score,food

--- src/snake.js
+++ src/snake.js
@@ -1,34 +1,113 @@
+//! snake
 let snake = document.getElementById("snake");
-
-let OnPressKey = document.addEventListener("keydown", logKey);
+//!speedGame
+let speed = 20;
+let direction = "none";
 let position = {
   x: 200,
   y: 200,
 };
 
-let speed = 10;
+let len = 1;
 
-function logKey(e) {
-  console.log(e.code);
+let food_position = {
+  x: 0,
+  y: 0,
+};
+//! get boardGame
+let boardGame = document.getElementById("mainBoard");
+
+const generateFood = () => {
+  let food = document.getElementById("food");
+  let x = Math.ceil(Math.random() * 20) * 20;
+  let y = Math.ceil(Math.random() * 20) * 20;
+  food_position.x = x;
+  food_position.y = y;
+  console.log(food_position);
+  food.style.top = `${food_position.y}px `;
+  food.style.left = `${food_position.x}px `;
+};
+//! eat food
+const eatFood = () => {
+  if (position.x === food_position.x && position.y === food_position.y) {
+    generateFood();
+    len++;
+    document.getElementById(
+      "score"
+    ).innerHTML = `<div class="number" id="score">${(len - 1) * 10}</div>`;
+  }
+};
+//! handler function keyPress
+const handelOnPressKey = (e) => {
   switch (e.code) {
     case "KeyW":
     case "ArrowUp":
-      position.y -= speed;
+      direction = "up";
       break;
     case "KeyD":
     case "ArrowRight":
-      position.x += speed;
+      direction = "right";
       break;
     case "KeyS":
     case "ArrowDown":
-      position.y += speed;
+      direction = "down";
       break;
     case "KeyA":
     case "ArrowLeft":
-      position.x -= speed;
+      direction = "left";
     default:
       break;
   }
-  snake.style.marginTop = `${position.y}px`;
-  snake.style.marginLeft = `${position.x}px`;
+};
+//! event handler for control snake
+window.addEventListener("keydown", handelOnPressKey);
+function waitforme(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
+const move = async () => {
+  while (true) {
+    switch (direction) {
+      case "up":
+        position.y -= speed;
+        break;
+      case "right":
+        position.x += speed;
+        break;
+      case "down":
+        position.y += speed;
+        break;
+      case "left":
+        position.x -= speed;
+      default:
+        break;
+    }
+    gameOver();
+    snake.style.top = `${position.y}px`;
+    snake.style.left = `${position.x}px`;
+    eatFood();
+    await waitforme(65);
+  }
+};
+
+const gameOver = () => {
+  if (
+    position.x < 0 ||
+    position.x > 400 ||
+    position.y < 0 ||
+    position.y > 400
+  ) {
+    position.x = 200;
+    position.y = 200;
+    len = 1;
+    direction = "none";
+    document.getElementById(
+      "score"
+    ).innerHTML = `<div class="number" id="score">${(len - 1) * 10}</div>`;
+    generateFood();
+  }
+};
+
+generateFood();
+move();
