0a5ddf399c00d922e2bca9befadeb4a54aa09a3f
Mohsen <moh.1380.1393@gmail.com>
2022-01-22 10:06:02 +0330

    Menu

---
+++ components/menu/main/menu.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import styles from "./menu.module.scss";
+
+const Menu = ({ handelPlay }: any) => {
+  useEffect(() => {
+    /* View in fullscreen */
+  }, []);
+  const openFullscreen = () => {
+    handelPlay();
+    let elem = document.documentElement;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (
+      //@ts-ignore
+      elem.webkitRequestFullscreen
+    ) {
+      /* Safari */
+      //@ts-ignore
+      elem.webkitRequestFullscreen();
+    } else if (
+      //@ts-ignore
+      elem.msRequestFullscreen
+    ) {
+      /* IE11 */
+      //@ts-ignore
+      elem.msRequestFullscreen();
+    }
+  };
+  const contactMe = () => {
+    window.open("https://github.com/MohsenBg", "_blank");
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.btnContainer}>
+        <div className={styles.btn} onClick={openFullscreen}>
+          play
+        </div>
+        <div className={styles.btn} onClick={contactMe}>
+          contact me
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
